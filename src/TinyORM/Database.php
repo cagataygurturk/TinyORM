37ec3280575b97f10b767198e7519c7889e0e3fe
Cagatay Gurturk <cagatay@itusozluk.com>
2014-09-23 02:42:31 +0200

    Connection parameters change

--- src/TinyORM/Database.php
+++ src/TinyORM/Database.php
@@ -34,9 +34,9 @@ class Database {
                     throw new Exception("TinyORM configuration not set");
                 }
                 self::$objInstance = new PDO('mysql:host=' . self::$config['dbhost'] . ';dbname=' . self::$config['database'] . ';charset=UTF8', self::$config['dbuser'], self::$config['dbpass'], array(
-                    PDO::ATTR_PERSISTENT => true,
+                    //PDO::ATTR_PERSISTENT => true,
                     PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
-                    PDO::MYSQL_ATTR_DIRECT_QUERY => true,
+                    //PDO::MYSQL_ATTR_DIRECT_QUERY => true,
                     PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
                 );
                 self::$objInstance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
