e4d8a438cb739207386e720922c20ca9554c96f6
cguertuerk <cguertuerk@team.mobile.de>
2016-12-26 22:23:59 +0100

    Database adjustments

--- src/TinyORM/Database.php
+++ src/TinyORM/Database.php
@@ -39,10 +39,11 @@ class Database {
 
     private static function getConnection($host, $database, $user, $pass) {
         $con = new PDO('mysql:host=' . $host . ';dbname=' . $database, $user, $pass, array(
-            //PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_PERSISTENT => true,
             PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
             //PDO::MYSQL_ATTR_DIRECT_QUERY => true,
-            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
+            //PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+            )
         );
 
         $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
