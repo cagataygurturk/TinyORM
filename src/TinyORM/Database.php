49664bdfc51e242cf334190d78c623fc64ba5781
cguertuerk <cguertuerk@team.mobile.de>
2016-12-26 22:43:08 +0100

    Fix

--- src/TinyORM/Database.php
+++ src/TinyORM/Database.php
@@ -42,7 +42,7 @@ class Database {
             PDO::ATTR_PERSISTENT => true,
             PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
             //PDO::MYSQL_ATTR_DIRECT_QUERY => true,
-            //PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
             )
         );
 
